ccdd418fca95f4c10ca2ffa24057c694efbbc4e5
Ishan Dewangan <dewanganishaan150324@gmail.com>
2026-07-06 14:42:59 +0530

    chore(db): role column added to users table

---
+++ backend/migrations/1783328823412_add-roles-to-users.js
@@ -0,0 +1,32 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.sql(`
+        CREATE TYPE user_role AS ENUM ('user', 'admin');
+
+        ALTER TABLE users
+        ADD COLUMN role user_role DEFAULT 'user' NOT NULL;    
+    `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql(`
+        ALTER TABLE users 
+        DROP COLUMN role;    
+
+        DROP TYPE user_role;
+    `);
+};

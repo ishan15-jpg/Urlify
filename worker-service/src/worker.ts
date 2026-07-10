7ebf0f440601de2636e188432a97d6fea021905c
Ishan Dewangan <dewanganishaan150324@gmail.com>
2026-07-10 16:38:10 +0530

    chore: dockerized worker service

--- worker-service/src/worker.ts
+++ worker-service/src/worker.ts
@@ -3,8 +3,31 @@ config();
 import { emailWorker } from './shared/queues/email.worker';
 import { passwordResetWorker } from './shared/queues/password-reset.worker';
 import { logger } from './shared/utils/logger';
+import { createServer } from 'http';
 
 logger.info('Worker Service is starting up...');
+
+const server = createServer((req, res) => {
+  if (req.url === '/health') {
+    res.statusCode = 200;
+    res.end('OK');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+const PORT = process.env.PORT;
+
+if (!PORT) {
+    logger.error('PORT is not defined');
+    process.exit(1);
+}
+
+server.listen(PORT, () => {
+    logger.info(`Worker service is running on port ${PORT}`);
+});
+
 /**
  * Handle graceful shutdown of the worker process.
  * Closes the BullMQ worker connection cleanly on process termination signals.
